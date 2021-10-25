11832d37ffb21885d4320e80058066f99bb6f7f7
Emanuel Acevedo <emanuel.acevedo@ciclo.com.co>
2021-10-25 15:17:06 -0500

    Add two integers project

---
+++ AddTwoIntegers/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number_1,number_2, result;
+    cout << "Hello Welcome, please insert the first number:" << endl;
+    cin >> number_1;
+    cout << "Please insert the second number:" << endl;
+    cin >> number_2;
+    result = number_1 + number_2;
+    cout << "The result is:" << result << " :D" << endl;
+    return 0;
+}
